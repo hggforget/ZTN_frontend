85ec56cb2f6d02198e6ae1468edf50619d8386e1
hggforget <1242537651@qq.com>
2023-04-20 16:55:37 +0800

    可视化组件优化

---
+++ src/components/LogPages/LogStatistic.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <el-container>
+        <elIcons ></elIcons>
+        {}
+        <LogTableVue></LogTableVue>
+
+    </el-container>
+
+
+</template>
+
+<script setup lang="ts">
+import LogTableVue from './LogTable.vue';
+
+
+
+
+</script>
+
+
+<style scoped>
+
+</style>
